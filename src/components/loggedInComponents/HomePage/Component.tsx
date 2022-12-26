6e51aa2e82b8be60bb80371489619db0e2247fc3
arav06 <contact.arav06@gmail.com>
2022-12-26 17:21:36 +0530

    mereko sona hai

--- src/components/loggedInComponents/HomePage/Component.tsx
+++ src/components/loggedInComponents/HomePage/Component.tsx
@@ -207,7 +207,7 @@ const HomePage = (props) => {
             <span className={home.seeAll}>Sort by</span>
           </div>
           <div className={home.transactions}>
-            {transactionHistory?((transactionHistory).map((transaction, index) => (
+             {transactionHistory?((transactionHistory).map((transaction, index) => (
               <div key={index} className={home.transactionBox}>
                 <div className={home.transactionDetails}>
                   <svg
